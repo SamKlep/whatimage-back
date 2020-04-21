8f8ec08c4afef7fcdd8da749331bb78fd38ae593
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-04-21 13:34:00 -0500

    fixed keras problem, images now classifying

--- src/images/models.py
+++ src/images/models.py
@@ -9,22 +9,20 @@ class Image(models.Model):
     classfield = models.CharField(max_length=200, blank=True)
     uploaded = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
-    return "Image classified at {}".format(self.uploaded.strftime('%Y-%m-%d %H:%M'))
+    def __str__(self):
+        return "Image classfied at {}".format(self.uploaded.strftime('%Y-%m-%d %H:%M'))
 
-        
-def save(self, *args, **kwargs):
-    try:
-        img = load_img(self.picture, target_size=(299,299))
-        img_arry = img_to_array(img)
-        to_pred = np.expand_dims(img_arry, axis=0) # (1, 299, 299, 3)
-        prep = preprocess_input(to_pred)
-        model = InceptionResNetV2(weights='imagenet')
-        prediction = model.predict(prep)
-        decoded = decode_predictions(prediction)[0][0][1]
-        self.classified = str(decoded)
-        print('success')
-    except Exception as e:
-        print('classification failed', e)
-    super().save(*args, **kwargs)
-
+    def save(self, *args, **kwargs):
+        try:
+            img = load_img(self.picture, target_size=(299,299))
+            img_arry = img_to_array(img)
+            to_pred = np.expand_dims(img_arry, axis=0) #(1, 299, 299, 3)
+            prep = preprocess_input(to_pred)
+            model = InceptionResNetV2(weights='imagenet')
+            prediction = model.predict(prep)
+            decoded = decode_predictions(prediction)[0][0][1]
+            self.classfield = str(decoded)
+            print(decoded)
+        except Exception as e:
+            print('classification failed', e)
+        super().save(*args, **kwargs)
